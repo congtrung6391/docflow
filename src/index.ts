d79c0c9a3fbc6bcb188490b0e79aa2d9c0443ad0
congtrung6391 <congtrung6391@gmail.com>
2026-07-14 11:48:21 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@
  */
 
 import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
-import registerTools from "../tools";
+import registerTools from "./tools";
 import { registerDocflowCommands, type CommandState } from "./commands";
 import { registerDocflowEvents, type DocflowState } from "./events";
 import { loadConfig, saveConfig, nowISO, getProjectPath } from "./utils";
